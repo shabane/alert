3ecdb38d4a7b1d73404ec262b066c156841aa0db
shabane <m.mohamadshabane@gmail.com>
2021-06-25 20:00:48 +0430

    now we have a usage which if the user dont specify the message and the title it will be shown
    and we have a icon to when the alert show notify, icon will shown

--- alert.py
+++ alert.py
@@ -3,11 +3,17 @@ from notifypy import Notify
 import sys
 
 # get the message and title from cli
-title = sys.argv[0]
-msg = sys.argv[1]
+try:
+    title = sys.argv[1]
+    msg = sys.argv[2]
+except:
+    print('Usage:')
+    print('\talert [title] [message]')
+    sys.exit(1)
 
 # call the class and send the message
 nft = Notify()
 nft.title = title
 nft.message = msg
+nft.icon = './notify.png'
 nft.send()
