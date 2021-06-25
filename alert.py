a07006c04ee670339b836b7426c0ec552f8293f0
shabane <m.mohamadshabane@gmail.com>
2021-06-25 19:35:50 +0430

    the alert.py now get a title and a message then will notify them

--- alert.py
+++ alert.py
@@ -1,1 +1,13 @@
 #!/bin/python3
+from notifypy import Notify
+import sys
+
+# get the message and title from cli
+title = sys.argv[0]
+msg = sys.argv[1]
+
+# call the class and send the message
+nft = Notify()
+nft.title = title
+nft.message = msg
+nft.send()
